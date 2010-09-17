240932f27130ae89f74aa16c41286ade5a11b58b
Rahim Sonawalla <rsonawalla@gmail.com>
2010-09-17 11:00:42 -0700

    Created dashboard to view all calls made

--- index.php
+++ index.php
@@ -2,14 +2,50 @@
 	ob_start("ob_gzhandler");
 	
 	require_once('./include/include.php');
+	require_once('./include/twilio_config.php');
 
 	$dwoo = new Dwoo();
 	$tpl = new Dwoo_Template_File(TEMPLATE_DIR.'/index.tpl');
 	$data = new Dwoo_Data();
+	$server_timezone = new DateTimeZone(date_default_timezone_get());
+	$local_timezone = new DateTimeZone(TIMEZONE);
 
-	$output = 'Hello, World!';
+	try
+	{
+		$db = DB::conn();
 
-	$data->assign('output', $output);
+		$result = $db->Execute('SELECT * FROM '.DB::$calls);
+
+		$total_call_duration = 0;
+		$total_call_count = $result->RecordCount();
+
+		while ($row = $result->FetchRow())
+		{
+			$from = $row[DB::$calls_from];
+			$start_time = new DateTime($row[DB::$calls_start_time], $server_timezone);
+			$duration = ceil($row[DB::$calls_duration]/60);
+
+			$total_call_duration += $duration;
+
+			$start_time->setTimezone($local_timezone);
+
+			$call['from'] = format_phone_number($from);
+			$call['start_time'] = $start_time->getTimestamp();
+			$call['duration'] = $duration;
+
+			$calls[] = $call;
+		}
+	}
+	catch (ADODB_Exception $e)
+	{
+		$error = '<p class="error">An error occurred while connecting to the database. Please try again later.</p>';
+	}
+	
+	$data->assign('total_call_count', $total_call_count);
+	$data->assign('total_call_duration', $total_call_duration);
+	$data->assign('total_spent', money_format('%i', $total_call_duration * CALL_COST));
+	$data->assign('calls', $calls);
+	$data->assign('error', $error);
 
 	$dwoo->output($tpl, $data);
 ?>
--- templates/index.tpl
+++ templates/index.tpl
@@ -2,14 +2,35 @@
 <html lang="en">
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<title>Framework</title>
+		<title>Support Dashboard</title>
 		
 		<link rel="stylesheet" type="text/css" href="./templates/css/reset.css" media="screen">
 		<link rel="stylesheet" type="text/css" href="./templates/css/style.css" media="screen">
 	</head>
 	<body>
 		<div id="main">
-			<p>{$output}</p>
+			<div id="info_bar">
+				<h1>Support Calls</h1>
+				<h1>
+					{$total_call_count} {if $total_call_count == 1} call {else} calls {/if} / 
+					{$total_call_duration} {if $total_call_duration == 1} minute {else} minutes {/if} / 
+					${$total_spent} spent
+				</h1>
+			</div>
+			<table id="calls" cellspacing="0" summary="Support calls placed.">
+				<tr>
+					<th scope="col">Caller</th>
+					<th scope="col">Time</th>
+					<th scope="col">Duration</th>
+				</tr>
+				{loop $calls}
+				<tr>
+					<td>{$from}</td>
+					<td>{date_format $start_time "%B %e, %Y at %l:%M %p"}</td>
+					<td>{$duration} {if $duration == 1} minute {else} minutes {/if}</td>
+				</tr>
+				{/loop}
+			</table>
 		</div>
 	</body>
 </html>
